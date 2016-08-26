8ac0d8da4347419d03e94e725e8e21c9f349d1c9
Kathylynn Saboda <kl0305@users.noreply.github.com>
2016-08-26 12:00:42 -0700

    Update cachesolvecompletev2.R

--- cachesolvecompletev2.R
+++ cachesolvecompletev2.R
@@ -1,3 +1,5 @@
+##this file creates a the cache of the matrix
+##designated by the function
 makeCachematrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -9,6 +11,7 @@ makeCachematrix <- function(x = matrix()) {
   getsolve <- function() m
   list(set = set, get = get, setsolve = setsolve,  getsolve = getsolve)
 }
+##this second function solves thematrix for the inverse
 cachesolve<- function(x, ...) {
   m <- x$getsolve()
   if(!is.null(m)) {
